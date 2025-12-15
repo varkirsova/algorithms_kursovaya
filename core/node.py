07cfb807f099d86899e619638628403550761e6a
varkirsova <vvlebedeva06@gmail.com>
2025-12-15 15:33:16 +0300

    new update

--- core/node.py
+++ core/node.py
@@ -41,68 +41,68 @@ class Node:
             if self.value != '-':
                 raise ValueError(f"неподходящий унарный оператор: {self.value}")
 
-        # через рекурсию
-        def prefix(self):
-            if self.type == NodeType.NUMBER:
-                return str(self.value) # просто число - детей нет
+    # через рекурсию
+    def prefix(self):
+        if self.type == NodeType.NUMBER:
+            return str(self.value)  # просто число - детей нет
 
-            elif self.type == NodeType.UNARY_OPERATOR:
-                return f"- {self.right.prefix()}" # унарный минус - идем вправо рекурсией
-            elif self.type == NodeType.BINARY_OPERATOR:
-                return f"{self.value} {self.left.prefix()} {self.right.prefix()}" # оператор - обоих детей смотрим рекурсией
+        elif self.type == NodeType.UNARY_OPERATOR:
+            return f"- {self.right.prefix()}"  # унарный минус - идем вправо рекурсией
+        elif self.type == NodeType.BINARY_OPERATOR:
+            return f"{self.value} {self.left.prefix()} {self.right.prefix()}"  # оператор - обоих детей смотрим рекурсией
 
-        # то же самое зеркально
-        def postfix(self):
-            if self.type == NodeType.NUMBER:
-                return str(self.value)
-            elif self.type == NodeType.UNARY_OPERATOR:
-                return f"{self.right.postfix()} -"
+    # то же самое зеркально
+    def postfix(self):
+        if self.type == NodeType.NUMBER:
+            return str(self.value)
+        elif self.type == NodeType.UNARY_OPERATOR:
+            return f"{self.right.postfix()} -"
 
-            elif self.type == NodeType.BINARY_OPERATOR:
-                return f"{self.left.postfix()} {self.right.postfix()} {self.value}"
+        elif self.type == NodeType.BINARY_OPERATOR:
+            return f"{self.left.postfix()} {self.right.postfix()} {self.value}"
 
-        def infix(self):
-            if self.type == NodeType.NUMBER:
-                return str(self.value)
+    def infix(self):
+        if self.type == NodeType.NUMBER:
+            return str(self.value)
 
-            elif self.type == NodeType.UNARY_OPERATOR:
-                return f"-{self.right.infix()}"
+        elif self.type == NodeType.UNARY_OPERATOR:
+            return f"-{self.right.infix()}"
 
-            elif self.type == NodeType.BINARY_OPERATOR:
-                left_str = self.infix_balance_prior(self.left, self.value, is_left=True)
-                right_str = self.infix_balance_prior(self.right, self.value, is_left=False)
-                return f"{left_str} {self.value} {right_str}"
+        elif self.type == NodeType.BINARY_OPERATOR:
+            left_str = self.infix_balance_prior(self.left, self.value, is_left=True)
+            right_str = self.infix_balance_prior(self.right, self.value, is_left=False)
+            return f"{left_str} {self.value} {right_str}"
 
-        def infix_balance_prior(self, child, parent_op, is_left):
-            if child.type != NodeType.BINARY_OPERATOR:
-                return child.infix()
+    def infix_balance_prior(self, child, parent_op, is_left):
+        if child.type != NodeType.BINARY_OPERATOR:
+            return child.infix()
 
-            priorit = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3} # 1 - низкий, 3 - высокий
-            child_priority = priorit.get(child.value, 0)
-            parent_priority = priorit.get(parent_op, 0)
+        priorit = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}  # 1 - низкий, 3 - высокий
+        child_priority = priorit.get(child.value, 0)
+        parent_priority = priorit.get(parent_op, 0)
 
-            # все случаи
-            if child_priority < parent_priority:
-                return f"({child.infix()})"
+        # все случаи
+        if child_priority < parent_priority:
+            return f"({child.infix()})"
 
-            elif child_priority == parent_priority:
-                if parent_op == '^':  # в случае степени нужно скобки ставить в конец т е в правый операнд
-                    if is_left:
-                        return child.infix()
-                    else:
-                        return f"({child.infix()})"
-
-                elif parent_op in ('-', '/'):  # с этими знаками важна последовательность, поэтому всегда скобки
+        elif child_priority == parent_priority:
+            if parent_op == '^':  # в случае степени нужно скобки ставить в конец т е в правый операнд
+                if is_left:
+                    return child.infix()
+                else:
                     return f"({child.infix()})"
 
-                else:  # остальные - можно не ставить скобки (рез не меняется) но для красоты можно
-                    if is_left:
-                        return f"({child.infix()})"
-                    else:
-                        return child.infix()
+            elif parent_op in ('-', '/'):  # с этими знаками важна последовательность, поэтому всегда скобки
+                return f"({child.infix()})"
 
-            else:  # больший приоритет у ребенка - скобки не нужны
-                return child.infix()
+            else:  # остальные - можно не ставить скобки (рез не меняется) но для красоты можно
+                if is_left:
+                    return f"({child.infix()})"
+                else:
+                    return child.infix()
+
+        else:  # больший приоритет у ребенка - скобки не нужны
+            return child.infix()
 
 
     def is_leaf(self):
